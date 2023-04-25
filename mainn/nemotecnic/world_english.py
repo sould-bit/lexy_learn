01aa2cb38c8c371e54c5778443f3e04c4251ba5c
jhon <jnonstiiven@gmail.com>
2023-04-25 12:38:28 -0500

    eliminacion de palabras no encontrads en el dict

--- mainn/nemotecnic/world_english.py
+++ mainn/nemotecnic/world_english.py
@@ -1,8 +1,8 @@
 #%%
 import pandas as pd 
 from AyDictionary import AyDictionary
-from pyphonetics import Soundex
 from translate import Translator
+from nltk.corpus import cmudict
 
 df = pd.read_csv('20k.txt', header= None,   names=['word'])
 
@@ -10,13 +10,18 @@ df = pd.read_csv('20k.txt', header= None,   names=['word'])
 def get_random():
     # seleccionamos aleatoriamente una palabra del data frame
     word = df.sample()['word'].values[0]
-    
     #obtener la definicion de la palabra 
     dictionary = AyDictionary(word)
     print(f"Buscando definicion para , {word}")
-
     dict = dictionary.getMeanings()
-    print("definicion  encontrada ", dict)
+    if word in dict and dict[word]:
+        print("definicion  encontrada ", dict)
+        # print(type(dict))
+    elif word  in  dict and dict[word] == None:
+        df.drop(df[df['word']== word].index, inplace= True)
+        print(f"se eliminara {word}")
+        df.to_csv("20k.txt", index=False, )
+        # return get_random()
     
     #traduccion con traslate
     print("traducciendo ", word )
@@ -27,13 +32,17 @@ def get_random():
     
     print("traduccion encontrada es", traslation)
         
-   
+    #obtener la trasnquipcion fonetica 
+    fonetic = cmudict.dict()
+    if word in fonetic:
+            phonetic = fonetic[word]
+            print("se encontro la  fonetica")
+    else:
+        print(f'la fonetica de {word} no se encontro')
+        phonetic = "Unknow"
     
-    #obtener la fonetica de la palabra 
-    soundex = Soundex()
-    print("Buscando fonetic para", word)
-    phonetic = soundex.phonetics(word)
-    print("fonetic  encontrada ", phonetic)
+
+    
     # devuelve la palabra , traduccion y fonetica 
     return {'word': word , 'form': dict,"traslation":traslation, 'phonetic': phonetic}
 
